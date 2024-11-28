cca602b3dbcd2a6a778711683bea394f0c792e3b
Richard Lim <s3901013@student.rmit.edu.au>
2024-11-28 16:25:16 +1100

    feat: operate() function

--- script.js
+++ script.js
@@ -18,3 +18,24 @@ const divide = (num1, num2) => {
     return result;
 }
 
+const Operations = {
+    Add: "+",
+    Subtract: "-",
+    Multiply: "*",
+    Divide: "/"
+}
+
+const operate = (operation, num1, num2) => {
+    switch(operation){
+        case Operations.Add:
+            return add(num1, num2);
+        case Operations.Subtract:
+            return subtract(num1, num2);
+        case Operations.Multiply:
+            return multiply(num1, num2);
+        case Operations.Divide:
+            return divide(num1, num2);
+    }
+}
+
+console.log(operate(Operations.Divide, 5, 3));   
