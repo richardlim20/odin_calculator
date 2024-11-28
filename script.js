bfce835c90fb6b67e160dc129f59cb810307c1e4
Richard Lim <s3901013@student.rmit.edu.au>
2024-11-28 17:27:22 +1100

    feat: connect dom

--- script.js
+++ script.js
@@ -38,4 +38,57 @@ const operate = (operation, num1, num2) => {
     }
 }
 
-console.log(operate(Operations.Divide, 5, 3));   
+const results = document.getElementById("results");
+let currentNumber = "";
+let previousNumber = "";
+let operation = "";
+let activeOperation = null;
+
+const calculatorContainer = document.getElementById("calculator-container");
+calculatorContainer.addEventListener("click", (e)=>{
+    const target = e.target;
+    if (target.classList.contains("number")){
+        if(target.textContent === "." && currentNumber.includes(".")||
+        target.textContent === "." && currentNumber === ""){}
+        else {
+            currentNumber += target.textContent;
+            results.textContent = currentNumber;
+        }
+    }
+
+    if(target.classList.contains("operator")){
+        if (currentNumber != ""){
+            operation = target.textContent;
+            target.classList.add("active");
+            activeOperation = target;
+            previousNumber = currentNumber;
+            currentNumber = "";
+        }
+        
+    }
+
+    if (target.classList.contains("action")){
+        const action = target.textContent;
+
+        if (action === "AC") {
+            currentNumber = "";
+            previousNumber = "";
+            operation = null;
+            results.textContent = "0";
+            activeOperation.classList.remove("active")
+        } 
+        else if (action === "DEL") {
+            currentNumber = currentNumber.slice(0, -1);
+            results.textContent = currentNumber || "0";
+        } 
+        else if (action === "=") {
+            if (currentNumber && previousNumber){
+                console.log(parseFloat(previousNumber), parseFloat(currentNumber), operation)
+                result = operate(operation, parseFloat(previousNumber), parseFloat(currentNumber));
+                currentNumber = result;
+                results.textContent = currentNumber;
+                activeOperation.classList.remove("active")
+            }
+        }
+    }
+})
