fd8af6f37e95762e60a90aa6c37f63c9572d1c75
Richard Lim <s3901013@student.rmit.edu.au>
2024-11-28 16:11:51 +1100

    feat: add basic functions

---
+++ script.js
@@ -0,0 +1,20 @@
+const add = (num1, num2) => {
+    result = num1 + num2;
+    return result;
+}
+
+const subtract = (num1, num2) => {
+    result = num1 - num2;
+    return result;
+}
+
+const multiply = (num1, num2) => {
+    result = num1 * num2;
+    return result;
+}
+
+const divide = (num1, num2) => {
+    result = num1 / num2;
+    return result;
+}
+
